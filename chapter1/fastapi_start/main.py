f3f211e083f7ef1b63afe8942f327db05cd10f66
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-01-30 16:32:50 +0500

    Chapter 1

---
+++ chapter1/fastapi_start/main.py
@@ -0,0 +1,9 @@
+import router_example
+from fastapi import FastAPI
+
+app = FastAPI()
+app.include_router(router_example.router)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
