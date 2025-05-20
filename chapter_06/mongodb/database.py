7f593b970daedb57d25cf75d1e58760c438e8e6c
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2025-05-20 17:50:18 +0500

    Databases and Asynchronous ORMs

---
+++ chapter_06/mongodb/database.py
@@ -0,0 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
+# Connection to the whole server
+motor_client = AsyncIOMotorClient("mongodb://localhost:27017")
+# Single database instance
+database = motor_client["chapter_06_mongo"]
+
+def get_database() -> AsyncIOMotorDatabase:
+    return database
