9af1de7b1f9f232da0efce336d742d347f0ddc99
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-10-21 09:38:42 -0400

    /usr/local/bin/python3 /home/cnc/Documents/FastAPI/LeetCode/remove-duplicates-from-sorted-array.py

---
+++ LeetCode/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        write_index = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[write_index] = nums[i]
+                write_index += 1
+        
+        # Дополнительно: заполнить оставшуюся часть (не обязательно по условию)
+        for i in range(write_index, len(nums)):
+            nums[i] = "_"  # или любое другое значение
+        
+        return write_index
+
+
+
+
+
+example = Solution()
+
+nums = [1,1,2]
+result = example.removeDuplicates(nums)
+print(result, nums)
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+result2 = example.removeDuplicates(nums)
+print(result2, nums)
+
+    
